61ab1c809fb934339c2ca1621b04d1dac40f9e50
nangeee <miniwoninuna@gmail.com>
2024-06-23 12:35:55 +0900

    Added bottom navigation bar

---
+++ app/src/main/java/edu/skku/cs/tastycart/CartActivity.kt
@@ -0,0 +1,13 @@
+package edu.skku.cs.tastycart
+
+import android.os.Bundle
+
+class CartActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+
+        // 장바구니 관련 기능 구현
+    }
+}
