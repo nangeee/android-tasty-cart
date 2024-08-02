f078f9ff808b4029e69ed560968571cc763a8eae
nangeee <miniwoninuna@gmail.com>
2024-08-02 10:49:07 +0900

    feat: add bottom navbar setting files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,9 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.google.code.gson:gson:2.10.1")
 
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.3.5")
+
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
